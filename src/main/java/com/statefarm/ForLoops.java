5623b233b0e1e1ef2cdaefd4168816c7660c2eae
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-08-16 14:35:27 -0700

    created the CommandLineArgs Class

---
+++ src/main/java/com/statefarm/ForLoops.java
@@ -0,0 +1,8 @@
+package com.statefarm;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        for(int i = 2; i <= 6; i+=2) System.out.println(i);
+    }
+
+}
---
+++ src/main/java/com/statefarm/WhileLoops.java
@@ -0,0 +1,15 @@
+package com.statefarm;
+
+import java.sql.SQLOutput;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        int num = 45;
+
+        while(num < 50) {
+            System.out.println(num);
+            num++;
+        }
+        System.out.println(num);
+    }
+}
