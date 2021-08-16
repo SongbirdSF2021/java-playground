3c134d9d5b69901e4677be5daddb1acd3b828a86
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-08-16 15:47:19 -0700

    added CommandLineArgs.java code

--- src/main/java/com/statefarm/CommandLineArgs.java
+++ src/main/java/com/statefarm/CommandLineArgs.java
@@ -1,4 +1,15 @@
 package com.statefarm;
 
 public class CommandLineArgs {
+    public static void main(String[] args) {
+        if(args.length != 2) {
+            System.out.println("You must pass two arguments and they both must be incased in double quotes.");
+            System.exit(0);
+        }
+
+        String name = String.format("%s", args[0]);
+        String email = String.format(" <%s>", args[1]);
+
+        System.out.println(name + email);
+    }
 }
