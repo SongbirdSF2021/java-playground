5b1a47a83233ae48c1f700830b661f81d37f4b42
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-08-23 10:41:57 -0700

    added example for sorting Array Lists

--- src/main/java/com/statefarm/SortArrayList.java
+++ src/main/java/com/statefarm/SortArrayList.java
@@ -1,2 +1,17 @@
-package com.statefarm;public class SortArrayList {
+package com.statefarm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SortArrayList {
+    //ArrayList<String> sortArray = new ArrayList<>();
+    public static void main(String[] args) {
+       ArrayList<String> sortedArgs = sortArray(args);
+        System.out.println(sortedArgs.get(0));
+    }
+    public static ArrayList<String> sortArray(String[] inputArray) {
+        ArrayList<String> sortedArray = new ArrayList<>(Arrays.asList(inputArray));
+        sortedArray.sort(null);
+        return sortedArray;
+    }
 }
